28a409fd9a1eeabf6830a4282de47625ac55b528
Miguel Doiche <migueldoiche2@gmail.com>
2023-04-19 16:11:44 -0300

    findById implementation

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -7,12 +7,9 @@ import model.entities.Department;
 public interface DepartmentDao {
 
 	void insert(Department obj);
-	void uptade(Department obj);
 	void deleteById(Integer id);
+	void uptade(Department obj);
 	Department findById(Integer id);
 	List<Department> findAll();
-	
-	
-
-
 }
+
